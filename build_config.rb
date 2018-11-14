78abdd46c4ca2a6c02fc3eb3d7690591c19dea1a
matsumotory <matsu1229@gmail.com>
2018-11-14 03:10:33 +0000

    Remove unused mrbgem

--- build_config.rb
+++ build_config.rb
@@ -4,6 +4,5 @@ MRuby::Build.new do |conf|
   conf.gem :github => 'iij/mruby-dir'
   conf.gem :github => 'matsumoto-r/mruby-cgroup'
   conf.gem :github => 'matsumoto-r/mruby-eventfd'
-  conf.gem :github => 'ksss/mruby-signal'
   conf.gem './'
 end
